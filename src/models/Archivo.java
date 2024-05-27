6f5fe55742d55d2dc02580f69cd47cc8686b94e2
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-05-27 14:59:22 -0600

    final

--- src/models/Archivo.java
+++ src/models/Archivo.java
@@ -1,23 +1,38 @@
 package models;
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+
 public class Archivo {
 
     public void escribirInformacion(ArbolBinario arbol, String nombreArchivo) {
         try (FileWriter fichero = new FileWriter(nombreArchivo)){
-            PrintWriter pw= new PrintWriter(fichero);
-            escribirNodo(arbol.getRaiz(), pw);
-        } catch (IOException e){
+            PrintWriter pw = new PrintWriter(fichero);
+            escribirConDireccion(arbol.getRaiz(), pw);
+            escribirSinDireccion(arbol.getRaiz(), pw);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void escribirNodo(Nodo nodo, PrintWriter pw) {
-        if (nodo !=null) {
-            pw.println(nodo.getEmployee());
-            escribirNodo(nodo.getIzquierdo(), pw);
-            escribirNodo(nodo.getDerecho(), pw);
+    private void escribirConDireccion(Nodo nodo, PrintWriter pw) {
+        if (nodo != null) {
+            if (!nodo.getEmployee().getDirection().isEmpty()) {
+                pw.println(nodo.getEmployee());
+            }
+            escribirConDireccion(nodo.getIzquierdo(), pw);
+            escribirConDireccion(nodo.getDerecho(), pw);
+        }
+    }
+
+    private void escribirSinDireccion(Nodo nodo, PrintWriter pw) {
+        if (nodo != null) {
+            if (nodo.getEmployee().getDirection().isEmpty()) {
+                pw.println(nodo.getEmployee());
+            }
+            escribirSinDireccion(nodo.getIzquierdo(), pw);
+            escribirSinDireccion(nodo.getDerecho(), pw);
         }
     }
 }
